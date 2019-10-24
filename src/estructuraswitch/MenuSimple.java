a17d87b30185361ac1062c63b30aa38a391cbb6c
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-10-24 10:06:52 +0200

    DOWHILE

--- src/estructuraswitch/MenuSimple.java
+++ src/estructuraswitch/MenuSimple.java
@@ -18,45 +18,51 @@ public class MenuSimple {
         String opcionSalida = "e";
         String opcion;
         System.out.println("Bienvenido a la aplicación del menú");
-        System.out.println("a.Menú General");
-        System.out.println("b.Menú de Audio");
-        System.out.println("c.Menú de Vídeo");
-        System.out.println("d.Controles");
-        System.out.println("e.Salir");
-        System.out.println("Introduce la letra del menú al que quieras acceder");
-        opcion = sc.next();
+        do {
+            System.out.println("a.Menú General");
+            System.out.println("b.Menú de Audio");
+            System.out.println("c.Menú de Vídeo");
+            System.out.println("d.Controles");
+            System.out.println("e.Salir");
+            System.out.println("Introduce la letra del menú al que quieras acceder");
+            opcion = sc.next();
 
-        switch (opcion.toLowerCase()) {
+            switch (opcion.toLowerCase()) {
 
-            case "a":
-                System.out.println("La opcion seleccionada es: A");
-                System.out.println("Accediendo a Menú General");
-                break;
-            case "b":
-                System.out.println("La opcion seleccionada es: B");
-                System.out.println("Accediendo a Menú de Audio");
-                break;
-            case "c":
-                System.out.println("La opcion seleccionada es: C");
-                System.out.println("Accediendo a Menú de Vídeo");
-                break;
-            case "d":
-                System.out.println("La opcion seleccionada es: D");
-                System.out.println("Accediendo a Controles");
-                break;
-            case "e":
-                System.out.println("La opcion seleccionada es: E");
-                System.out.println("Saliendo del menú");
-                break;
-            default:
-                System.out.println("Opción no contemplada");
-                do{
+                case "a":
+                    System.out.println("La opcion seleccionada es: A");
+                    System.out.println("Accediendo a Menú General");
+                    System.out.println(" ");
+                    break;
+                case "b":
+                    System.out.println("La opcion seleccionada es: B");
+                    System.out.println("Accediendo a Menú de Audio");
+                    System.out.println(" ");
+                    break;
+                case "c":
+                    System.out.println("La opcion seleccionada es: C");
+                    System.out.println("Accediendo a Menú de Vídeo");
+                    System.out.println(" ");
+                    break;
+                case "d":
+                    System.out.println("La opcion seleccionada es: D");
+                    System.out.println("Accediendo a Controles");
+                    System.out.println(" ");
+                    break;
+                case "e":
+                    System.out.println("La opcion seleccionada es: E");
+                    System.out.println("Saliendo del menú");
+                    System.out.println(" ");
+                    break;
+                default:
+                    System.out.println("Opción no contemplada");
+
                     System.out.println("Introduce otra letra");
-                    opcion=sc.next();
-                    
-                }while(!opcion.equalsIgnoreCase(opcionSalida));
-                     
-        }
+                    System.out.println(" ");
+                    opcion = sc.next();
+
+            }
+        } while (!opcion.equalsIgnoreCase(opcionSalida));
 
     }
 
